7e4f5ee8094cb49723bff112088f795569c5cefd
dyadee <jadefernandez81@gmail.com>
2017-09-03 10:04:49 +0800

    Resolving jsp rendering

--- springboot_demo/src/main/java/com/barehacks/durianbite/SpringbootDemoApplication.java
+++ springboot_demo/src/main/java/com/barehacks/durianbite/SpringbootDemoApplication.java
@@ -2,11 +2,19 @@ package com.barehacks.durianbite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootDemoApplication {
+public class SpringbootDemoApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootDemoApplication.class);
+	}
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws Exception{
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
 }
--- springboot_demo/src/main/java/com/barehacks/durianbite/controllers/HelloSpringBoot.java
+++ springboot_demo/src/main/java/com/barehacks/durianbite/controllers/HelloSpringBoot.java
@@ -1,15 +1,34 @@
 package com.barehacks.durianbite.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+//public class HelloSpringBoot {
+//	@RequestMapping("/")
+//	public String home(Model model){
+//		return "home";
+//	}
+//
+//}
+
+@Controller
 public class HelloSpringBoot {
-	@RequestMapping(value="/")
-	@ResponseBody
-	public String helloboot(){
-		return "Hello Spring Boot in STS and Github!";
+	@RequestMapping("/")
+	public String home(Model model){
+		return "home";
 	}
 
 }
+//@Controller
+//public class HelloSpringBoot {
+//
+//   @RequestMapping("/")
+//   public String handler (Model model) {
+//       model.addAttribute("msg",
+//                          "a jar packaging example");
+//       return "myView";
+//   }
+//}
